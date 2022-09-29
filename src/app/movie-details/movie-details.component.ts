eb11a8747dfc5ae437ea61ed63a999c44681f6eb
thanasismak <makris_th@hotmail.com>
2022-09-29 10:42:01 +0300

    v5

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,39 +1,44 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
-
+import { Subscription, tap } from 'rxjs';
+// import { tap } from 'rxjs/internal/operators/tap';
 import { ApiService } from '../api.service';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Inject } from '@angular/core';
 
-
 @Component({
   selector: 'movie-details',
   templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.scss']
+  styleUrls: ['./movie-details.component.scss'],
 })
 export class MovieDetailsComponent {
   routeParamsSubscription: Subscription;
-  constructor(private apiService: ApiService, private activatedRouter: ActivatedRoute,) {
-  }
-  postRating: { "value": number };
+  constructor(
+    private apiService: ApiService,
+    private activatedRouter: ActivatedRoute
+  ) {}
+  postRating: { value: number };
   rating: number;
   addTofav: boolean = false;
   @Input() selected: boolean;
   @Output() selectedChange = new EventEmitter<boolean>();
   detailedMovie;
 
+  // ngOnInit() {
+
+  // }
   ngOnInit() {
-    this.routeParamsSubscription = this.activatedRouter.params.subscribe((params) => {
-      let mid = params["movie_id"];
-      this.apiService.getMoviesDetailsByID(mid).subscribe((response) => {
-        this.detailedMovie = response;
-        console.log(response);
-      },
-        (error) => {
-          console.log(error)
-        });
-    })
+    this.routeParamsSubscription = this.activatedRouter.params.subscribe(
+      (params) => {
+        let mid = params['movie_id'];
+        this.apiService.getMoviesDetailsByID(mid).pipe(
+          tap((response) => {
+            response ? this.detailedMovie.next(response) : null;
+            console.log(response);
+          })
+        );
+      }
+    );
   }
 
   ngOnDestroy() {
@@ -44,19 +49,19 @@ export class MovieDetailsComponent {
     this.apiService.postRating(rating).subscribe(
       (response) => {
         location.reload();
-        console.log(response)
+        console.log(response);
       },
       (error) => {
-        console.log(error)
+        console.log(error);
       }
-    )
+    );
   }
-  
+
   addTofavorite(addTofav) {
-    console.log('inside addTofavorite function')
-    localStorage.setItem('true', JSON.stringify(this.addTofav))
+    console.log('inside addTofavorite function');
+    localStorage.setItem('true', JSON.stringify(this.addTofav));
   }
-  
+
   toggleSelected() {
     this.selected = !this.selected;
     this.selectedChange.emit(this.selected);
